51310fdbd12f631b9a6d4cf51f3df6c66cb33202
kimi <kimi@DESKTOP-HLSO57E>
2022-08-15 19:17:38 +0430

    Add project files.

---
+++ Pascal triangle1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        int[][] pascal = new int[rows][];
+        for (int a = 1; a <= rows; a++)
+        {
+            int[] row = new int[a];
+            row[0] = 1;
+            row[a - 1] = 1;
+            for (int b = 2; b < a; b++)
+            {
+                row[b - 1] = pascal[a - 2][b - 2] + pascal[a - 2][b - 1];
+            }
+            pascal[a - 1] = row;
+        }
+        return pascal;
+    }
+}
